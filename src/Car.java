32235e5adffc0ce7766c30645426dcdf26c666b0
Alice <alice.stelmach@gmail.com>
2018-05-19 09:22:23 +0200

    Stworzenie projektu

---
+++ src/Car.java
@@ -0,0 +1,15 @@
+public class Car {
+    String brand;
+    String model;
+    int year;
+    double capacity;
+
+    Car(String mar, String mod, int rok, double silnik){
+
+        brand = mar;
+        model=mod;
+        year=rok;
+        capacity =silnik;
+
+    }
+}
